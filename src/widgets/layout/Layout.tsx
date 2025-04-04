56c210ca0471cca0fb8ae0f2ae41d5b1c375370d
THE-GOOSE <nikitos54345@gmail.com>
2025-04-04 17:46:52 +0000

    fix

--- src/widgets/layout/Layout.tsx
+++ src/widgets/layout/Layout.tsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import {motion} from 'motion/react'
+import { NavLink, Outlet } from "react-router";
 
-const Layout = () => {
-
+export default function Layout() {
   return (
-
-    <div className='fixed w-screen h-screen'>
-        <motion.div></motion.div>
+    <div>
+      <div className="w-full h-[50px] bg-amber-300">
+        <div className="flex gap-5">
+          <NavLink to={"/"}>
+            <h2>Полезная информация</h2>
+          </NavLink>
+          <NavLink to={"/tasks"}>
+            <h2>Задачи</h2>
+          </NavLink>
+          <NavLink to={"/admin"}>
+            <h2>Администрирование</h2>
+          </NavLink>
+          <NavLink to={"/auth/login"}>
+            <h2>Регистрация</h2>
+          </NavLink>
+        </div>
+      </div>
+      <div>
+        <Outlet />
+      </div>
     </div>
-
-  )
-
+  );
 }
-
-export default Layout
