c13ff4c8b4bcf10a282171f7f27418453b5e7bea
Lyaguh NoteBook <artyom07206@gmail.com>
2025-04-04 18:27:28 +0300

    stage

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline font-roboto">Hello world!</h1>
     </>
   );
 }
