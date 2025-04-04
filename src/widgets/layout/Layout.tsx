6a5584bfd7297cd99acf4772d6ae5cfd371058e1
THE-GOOSE <nikitos54345@gmail.com>
2025-04-04 16:33:02 +0000

    start

---
+++ src/widgets/layout/Layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {motion} from 'motion/react'
+
+const Layout = () => {
+
+  return (
+
+    <div className='fixed w-screen h-screen'>
+        <motion.div></motion.div>
+    </div>
+
+  )
+
+}
+
+export default Layout
---
+++ src/widgets/layout/components/Logotype.tsx
@@ -0,0 +1,15 @@
+const Logotype = () => {
+
+  return (
+
+    <div className='h-full w-[166px]'>
+
+        <img src="https://upload.wikimedia.org/wikipedia/commons/9/9b/TTK_logo.svg" alt="hui" />
+
+    </div>
+
+  )
+
+}
+
+export default Logotype
