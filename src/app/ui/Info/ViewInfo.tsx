3335e7f91bcffb4c17f7760e8238e77c36ed6be7
Lyaguh NoteBook <artyom07206@gmail.com>
2025-04-05 20:40:23 +0300

    add context in task/add view info db

--- src/app/ui/Info/ViewInfo.tsx
+++ src/app/ui/Info/ViewInfo.tsx
@@ -4,38 +4,45 @@ import classNames from "classnames";
 import { IoPersonCircleOutline } from "react-icons/io5";
 interface IProps {
   setViewInfo: (arg: boolean) => void;
+  title: string;
+  content: string;
+  image: string;
+  updatedAt: string;
+  lastEditor: string;
 }
 
-const ViewInfo: React.FC<IProps> = ({ setViewInfo }) => {
+const ViewInfo: React.FC<IProps> = ({
+  setViewInfo,
+  title,
+  content,
+  image,
+  updatedAt,
+  lastEditor,
+}) => {
   return (
     <div className="flex flex-col gap-[20px] items-center px-[20px] py-5">
       <div className="flex justify-between gap-[20px] ">
         <div className=" flex flex-col justify-between gap-[20px]">
           <h2 className="p-[13px] w-[440px] h-[78px]  bg-modal-bg dark:bg-[#2E2E2E] rounded-[10px]">
-            Текст слайда
+            {title}
           </h2>
           <h2 className="p-[13px] w-[440px] h-[78px] bg-modal-bg dark:bg-[#2E2E2E] rounded-[10px]">
-            <div>Последнее обновление: 05.04.2025</div>
+            <div>Последнее обновление: {updatedAt}</div>
             <div className="flex gap-3">
               <IoPersonCircleOutline className="size-[24px] text-[24px]" />
-              Карпов степан
+              {lastEditor}
             </div>
           </h2>
         </div>
-        <div
+        <img
+          src={image}
           className={classNames(
             "bg-modal-bg dark:bg-[#2E2E2E] flex justify-center items-center size-50 rounded-xl overflow-hidden"
           )}
         />
       </div>
       <div className="bg-modal-bg dark:bg-[#2E2E2E] rounded-[10px] w-[659px] h-[383px] p-[13px]">
-        Основы игры: Постановка рук и пальцев. Кисти должны быть расслабленными,
-        а пальцы — слегка изогнутыми, чтобы равномерно распределить нагрузку.
-        Рекомендуется держать руки так, будто в кисти лежит мяч или яблоко.
-        Важно работать над всеми пальцами, включая мизинец и большой палец. 12
-        Правильная позиция за инструментом. Стул должен быть достаточно высоким,
-        чтобы руки находились на уровне клавиш. Нужно сидеть ближе к краю стула,
-        чтобы спина оставалась прямой, а ноги устойчиво стояли на полу. 1
+        {content}
       </div>
       <Button
         className="w-full"
--- src/app/ui/Tasks/TaskTemplate.tsx
+++ src/app/ui/Tasks/TaskTemplate.tsx
@@ -1,6 +1,9 @@
 import cn from "classnames";
+import { useState } from "react";
 import { FaEllipsisVertical } from "react-icons/fa6";
 import { IoPersonCircleOutline } from "react-icons/io5";
+import ContextWindow from "../global/ContextWindow";
+import { GoTriangleLeft } from "react-icons/go";
 interface TaskTemplateProps {
   title: string;
   priority: string;
@@ -18,22 +21,58 @@ export default function TaskTemplate({
     plannedDate
   );
 
+  const [visibleContextMenu, setVisibleContextMenu] = useState(false);
+  const [visibleChangeCategory, setVisibleChangeCategory] = useState(false);
+
   return (
     <div
       className={cn(
-        "w-full dark:bg-[#202020] p-[20px] border-[2px] rounded-[16px] shadow-lg",
+        "w-full dark:bg-[#202020] p-[20px] border-[2px] rounded-[16px] shadow-lg relative",
         {
           [" border-main"]: priority === "Высокий",
           [" border-[#c7c22a]"]: priority === "Средний",
           [" border-[#21a637]"]: priority === "Низкий",
         }
       )}
+      onClick={() => {
+        setVisibleChangeCategory(false);
+        setVisibleContextMenu(false);
+      }}
     >
-      <div className="flex justify-between">
+      <ContextWindow visible={visibleContextMenu}>
+        <h2
+          className="flex gap-[6px] cursor-pointer"
+          onClick={() => {
+            setVisibleChangeCategory(!visibleChangeCategory);
+          }}
+        >
+          <GoTriangleLeft className="self-center " />
+          Сменить статус
+        </h2>
+        <h2 className="cursor-pointer">Редактировать</h2>
+        <h2 className="text-main cursor-pointer">Удалить</h2>
+      </ContextWindow>
+      <ContextWindow
+        className="right-[170px] z-20"
+        visible={visibleChangeCategory}
+      >
+        <h2 className="cursor-pointer">В работе</h2>
+        <h2 className="cursor-pointer">Выполнено</h2>
+      </ContextWindow>
+      <div className="flex justify-between ">
         <h2>
           Название: <b>{title}</b>
         </h2>
-        <FaEllipsisVertical className="self-center mr-[5px]" />
+        <FaEllipsisVertical
+          className="self-center mr-[5px] "
+          onClick={(e: any) => {
+            e.stopPropagation();
+            setVisibleContextMenu(!visibleContextMenu);
+            if (visibleChangeCategory) {
+              setVisibleChangeCategory(false);
+            }
+          }}
+        />
       </div>
 
       <h2>
