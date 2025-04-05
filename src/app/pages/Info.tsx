c6174a7709ea4c064ae0c3942fd0e41e3d9a7a83
Lyaguh NoteBook <artyom07206@gmail.com>
2025-04-05 16:32:56 +0300

    features and fixs

--- src/app/pages/Info.tsx
+++ src/app/pages/Info.tsx
@@ -6,42 +6,71 @@ import { postsSlice } from "../model/store";
 import { useEffect, useState } from "react";
 import ModalWindow from "../ui/global/ModalWindow";
 import CreateModal from "../ui/Info/CreateModal";
+import classNames from "classnames";
+import { MdErrorOutline } from "react-icons/md";
 
 export default function Info() {
+  const [createModal, setCreateModal] = useState<boolean>(false);
 
-  const [createModal, setCreateModal] = useState<boolean>(false)
+  const postSlice = postsSlice();
 
-  const postSlice = postsSlice()
-
-  useEffect(() => {postSlice.fetchPosts()}, [])
+  useEffect(() => {
+    postSlice.fetchPosts();
+  }, []);
 
   return (
     <>
-    <div className="px-[30px] flex flex-col h-[calc(100vh-74px)] overflow-y-scroll scroll ">
-      <div className="py-[10px] lg:py-[30px] flex justify-between">
-        <Button
-          appearance="grayButton"
-          className="flex gap-[14px] items-center"
-          onClick={() => setCreateModal(true)}
+      <div className="px-[30px] flex flex-col h-[calc(100vh-74px)] overflow-y-scroll scroll ">
+        <div className="py-[10px] lg:py-[30px] flex justify-between">
+          <Button
+            appearance="grayButton"
+            className="flex gap-[14px] items-center"
+            onClick={() => setCreateModal(true)}
+          >
+            <FaPlus />
+            Создать статью
+          </Button>
+          <Button
+            appearance="grayButton"
+            className="flex gap-[14px] items-center"
+          >
+            <FaHistory />
+            История изменений
+          </Button>
+        </div>
+        <div
+          className={classNames(
+            "h-[700px] lg:h-[855px] scroll gap-[6px]   inline-flex flex-wrap ",
+            {
+              ["overflow-y-scroll"]: postSlice.posts.length > 3,
+            }
+          )}
         >
-          <FaPlus />
-          Создать статью
-        </Button>
-        <Button
-          appearance="grayButton"
-          className="flex gap-[14px] items-center"
-        >
-          <FaHistory />
-          История изменений
-        </Button>
+          {postSlice.posts.length === 0 && (
+            <div className="w-full h-full flex gap-2.5 justify-center items-center dark:text-white">
+              <MdErrorOutline className="size-[30px]" />
+              <h2 className="text-[30px]">
+                Ничего не найдено, попробуйте добавить новую статью
+              </h2>
+            </div>
+          )}
+          {postSlice.posts.length > 0 &&
+            postSlice.posts.map((post) => (
+              <PostTemplate
+                id={Number(post.id)}
+                title={post.title}
+                content={post.content}
+                author={post.lastEditor}
+                createdAt={post.createdAt}
+                image={post.image}
+                key={post.id}
+              />
+            ))}
+        </div>
       </div>
-      <div className="h-[700px] lg:h-[855px] scroll gap-[6px] overflow-y-scroll inline-flex flex-wrap ">
-        {postSlice.posts.map((post) => <PostTemplate id={Number(post.id)} title={post.title} content={post.content} author={post.lastEditor} createdAt={post.createdAt} image={post.image} key={post.id}/>)}
-      </div>
-    </div>
-    <ModalWindow isShow={createModal}>
-      <CreateModal setCreateModal={setCreateModal}/>
-    </ModalWindow>
+      <ModalWindow isShow={createModal}>
+        <CreateModal setCreateModal={setCreateModal} />
+      </ModalWindow>
     </>
   );
 }
